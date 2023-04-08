a495b7155bf76e21937f67c596b80aafa12aa533
semami <setareh.emami@gmail.com>
2023-04-08 09:25:40 +0330

    exception handle

--- src/main/java/com/nicico/evaluation/service/GroupTypeService.java
+++ src/main/java/com/nicico/evaluation/service/GroupTypeService.java
@@ -4,16 +4,21 @@ import com.nicico.copper.common.domain.criteria.NICICOCriteria;
 import com.nicico.copper.common.domain.criteria.SearchUtil;
 import com.nicico.copper.common.dto.grid.TotalResponse;
 import com.nicico.evaluation.dto.GroupTypeDTO;
+import com.nicico.evaluation.exception.ApplicationException;
+import com.nicico.evaluation.exception.ServiceException;
 import com.nicico.evaluation.iservice.IGroupTypeService;
 import com.nicico.evaluation.mapper.GroupTypeMapper;
 import com.nicico.evaluation.model.GroupType;
 import com.nicico.evaluation.repository.GroupTypeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.nicico.evaluation.exception.CoreException.NOT_FOUND;
 
 
 @RequiredArgsConstructor
@@ -23,12 +28,13 @@ public class GroupTypeService implements IGroupTypeService {
 
     private final GroupTypeRepository repository;
     private final GroupTypeMapper mapper;
+    private final ApplicationException<ServiceException> applicationException;
 
     @Override
     @Transactional(readOnly = true)
     @PreAuthorize("hasAuthority('R_GROUP_TYPE')")
     public GroupTypeDTO.Info get(Long id) throws Exception {
-        GroupType groupType = repository.findById(id).orElseThrow(() -> new Exception("exception.main-desktop.record.not.found"));
+        GroupType groupType = repository.findById(id).orElseThrow(() -> applicationException.createApplicationException(NOT_FOUND, HttpStatus.NOT_FOUND));
         return mapper.entityToDtoInfo(groupType);
     }
 
@@ -60,7 +66,7 @@ public class GroupTypeService implements IGroupTypeService {
     @Transactional
     @PreAuthorize("hasAuthority('U_GROUP_TYPE')")
     public GroupTypeDTO.Info update(GroupTypeDTO.Update dto) {
-        GroupType kPIType = repository.findById(dto.getId()).orElseThrow(() -> new RuntimeException("GroupType not  found"));
+        GroupType kPIType = repository.findById(dto.getId()).orElseThrow(() -> applicationException.createApplicationException(NOT_FOUND, HttpStatus.NOT_FOUND));
         mapper.update(kPIType, dto);
         GroupType save = repository.save(kPIType);
         return mapper.entityToDtoInfo(save);
@@ -70,7 +76,7 @@ public class GroupTypeService implements IGroupTypeService {
     @Transactional
     @PreAuthorize("hasAuthority('D_GROUP_TYPE')")
     public void delete(Long id) {
-        GroupType kPIType = repository.findById(id).orElseThrow(() -> new RuntimeException("GroupType not  found"));
+        GroupType kPIType = repository.findById(id).orElseThrow(() -> applicationException.createApplicationException(NOT_FOUND, HttpStatus.NOT_FOUND));
         repository.delete(kPIType);
     }
 
--- src/main/java/com/nicico/evaluation/service/MeritComponentTypeService.java
+++ src/main/java/com/nicico/evaluation/service/MeritComponentTypeService.java
@@ -4,16 +4,21 @@ import com.nicico.copper.common.domain.criteria.NICICOCriteria;
 import com.nicico.copper.common.domain.criteria.SearchUtil;
 import com.nicico.copper.common.dto.grid.TotalResponse;
 import com.nicico.evaluation.dto.MeritComponentTypeDTO;
+import com.nicico.evaluation.exception.ApplicationException;
+import com.nicico.evaluation.exception.ServiceException;
 import com.nicico.evaluation.iservice.IMeritComponentTypeService;
 import com.nicico.evaluation.mapper.MeritComponentTypeMapper;
 import com.nicico.evaluation.model.MeritComponentType;
 import com.nicico.evaluation.repository.MeritComponentTypeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.nicico.evaluation.exception.CoreException.NOT_FOUND;
 
 
 @RequiredArgsConstructor
@@ -23,13 +28,14 @@ public class MeritComponentTypeService implements IMeritComponentTypeService {
 
     private final MeritComponentTypeRepository repository;
     private final MeritComponentTypeMapper mapper;
+    private final ApplicationException<ServiceException> applicationException;
 
     @Override
     @Transactional(readOnly = true)
     @PreAuthorize("hasAuthority('R_MERIT_COMPONENT_TYPE')")
 
     public MeritComponentTypeDTO.Info get(Long id) throws Exception {
-        MeritComponentType meritComponentType = repository.findById(id).orElseThrow(() -> new Exception("exception.record.not.found"));
+        MeritComponentType meritComponentType = repository.findById(id).orElseThrow(() -> applicationException.createApplicationException(NOT_FOUND, HttpStatus.NOT_FOUND));
         return mapper.entityToDtoInfo(meritComponentType);
     }
 
@@ -61,7 +67,7 @@ public class MeritComponentTypeService implements IMeritComponentTypeService {
     @Transactional
     @PreAuthorize("hasAuthority('U_MERIT_COMPONENT_TYPE')")
     public MeritComponentTypeDTO.Info update(MeritComponentTypeDTO.Update dto) {
-        MeritComponentType meritComponentType = repository.findById(dto.getId()).orElseThrow(() -> new RuntimeException("MeritComponentType not  found"));
+        MeritComponentType meritComponentType = repository.findById(dto.getId()).orElseThrow(() -> applicationException.createApplicationException(NOT_FOUND, HttpStatus.NOT_FOUND));
         mapper.update(meritComponentType, dto);
         MeritComponentType save = repository.save(meritComponentType);
         return mapper.entityToDtoInfo(save);
@@ -71,7 +77,7 @@ public class MeritComponentTypeService implements IMeritComponentTypeService {
     @Transactional
     @PreAuthorize("hasAuthority('D_MERIT_COMPONENT_TYPE')")
     public void delete(Long id) {
-        MeritComponentType meritComponentType = repository.findById(id).orElseThrow(() -> new RuntimeException("MeritComponentType not  found"));
+        MeritComponentType meritComponentType = repository.findById(id).orElseThrow(() -> applicationException.createApplicationException(NOT_FOUND, HttpStatus.NOT_FOUND));
         repository.delete(meritComponentType);
     }
 
