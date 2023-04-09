4879882c7c4de6e4f3a36b7e816ec6df69788566
xnamdari <you@example.com>
2023-04-09 08:08:15 +0330

    change redirect address

--- src/main/java/com/nicico/evaluation/config/CommunicateUIResource.java
+++ src/main/java/com/nicico/evaluation/config/CommunicateUIResource.java
@@ -66,7 +66,10 @@ public class CommunicateUIResource {
 
         String accessToken = client.getAccessToken().getTokenValue();
         httpServletResponse.setHeader(HttpHeaders.AUTHORIZATION, accessToken);
-        httpServletResponse.setHeader(HttpHeaders.LOCATION, uiRedirectAddress);
+        httpServletResponse.setHeader(HttpHeaders.LOCATION, uiRedirectAddress +
+                "?token= Bearer " + accessToken + "&" +
+                "landingAddress=" + uiLandingAddress + "&" +
+                "userNationalCode=" + SecurityUtil.getNationalCode());
         return new ResponseEntity<>(HttpStatus.MOVED_PERMANENTLY);
     }
 
