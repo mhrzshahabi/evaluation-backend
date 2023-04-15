b5c32cbd23977e5aa07342087e0d0b18bdc2452f
semami <setareh.emami@gmail.com>
2023-04-15 14:51:49 +0330

    change dto

--- src/main/java/com/nicico/evaluation/repository/MeritComponentTypeRepository.java
+++ src/main/java/com/nicico/evaluation/repository/MeritComponentTypeRepository.java
@@ -1,6 +1,5 @@
 package com.nicico.evaluation.repository;
 
-import com.nicico.evaluation.model.MeritComponent;
 import com.nicico.evaluation.model.MeritComponentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
