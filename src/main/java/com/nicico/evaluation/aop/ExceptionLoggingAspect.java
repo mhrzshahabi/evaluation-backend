b6efb2b57aa9dfaa01e9b2bbee306e3192aa0fca
amir lotfi <lotfiamir137272@gmail.com>
2023-04-16 12:36:19 +0430

    aop for logging exception all method

---
+++ src/main/java/com/nicico/evaluation/aop/ExceptionLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.nicico.evaluation.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ExceptionLoggingAspect {
+
+    @Pointcut("within(com.nicico..*) && execution(* *(..))")
+    public void matchAllMethods(){}
+
+    @AfterThrowing(pointcut = "logPointcut()", throwing = "ex")
+    public void afterThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info(joinPoint.getSignature() + " --> " + String.format("Exception name: %s __ Exception message: %s", ex.getClass().getName(), ex.getMessage() ) );
+    }
+
+}
