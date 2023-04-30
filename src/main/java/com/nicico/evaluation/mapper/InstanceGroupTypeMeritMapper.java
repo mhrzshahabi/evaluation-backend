edb32535bec24cee8e6be4424a0f652039f5db17
xnamdari <you@example.com>
2023-04-30 15:04:58 +0330

    add delete exception

--- src/main/java/com/nicico/evaluation/mapper/InstanceGroupTypeMeritMapper.java
+++ src/main/java/com/nicico/evaluation/mapper/InstanceGroupTypeMeritMapper.java
@@ -3,7 +3,9 @@ package com.nicico.evaluation.mapper;
 import com.nicico.evaluation.dto.InstanceGroupTypeMeritDTO;
 import com.nicico.evaluation.model.InstanceGroupTypeMerit;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
@@ -14,9 +16,19 @@ public interface InstanceGroupTypeMeritMapper {
 
     InstanceGroupTypeMeritDTO.Info entityToDtoInfo(InstanceGroupTypeMerit entity);
 
+    @Mappings({
+            @Mapping(target = "id", source = "instance.id"),
+            @Mapping(target = "title", source = "instance.title"),
+    })
+    InstanceGroupTypeMeritDTO.InstanceTupleDTO  entityToTupleDTOInfo(InstanceGroupTypeMerit entity);
+
     List<InstanceGroupTypeMeritDTO.Info> entityToDtoInfoList(List<InstanceGroupTypeMerit> entities);
 
     List<InstanceGroupTypeMeritDTO.InstanceInfo> entityToDtoInstanceList(List<InstanceGroupTypeMerit> entities);
+
+    List<InstanceGroupTypeMeritDTO.InstanceTupleDTO> entityToTupleDTOInstanceList(List<InstanceGroupTypeMerit> entities);
+
+//    List<InstanceGroupTypeMeritDTO.InstanceTupleDTO>
 
     void update(@MappingTarget InstanceGroupTypeMerit entity, InstanceGroupTypeMeritDTO.Update dto);
 }
