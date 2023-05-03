cc9c7661780b15d6405f2698ab215757e8a2c261
amir lotfi <lotfiamir137272@gmail.com>
2023-05-03 12:01:01 +0430

    change special case coumnNames

--- src/main/java/com/nicico/evaluation/dto/SpecialCaseDTO.java
+++ src/main/java/com/nicico/evaluation/dto/SpecialCaseDTO.java
@@ -17,15 +17,13 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SpecialCaseDTO {
 
-    private String firstName;
+    private String assessFullName;
 
-    private String lastName;
+    private String assessNationalCode;
 
-    private String nationalCode;
+    private String assessPostCode;
 
-    private String postCode;
-
-    private String assessorName;
+    private String assessorFullName;
 
     private String assessorNationalCode;
 
