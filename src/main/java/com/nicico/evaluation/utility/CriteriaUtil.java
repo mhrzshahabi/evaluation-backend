c515eece010a936456db219abc6981b60af28a25
semami <setareh.emami@gmail.com>
2023-05-06 15:48:13 +0330

    create post merit instance

--- src/main/java/com/nicico/evaluation/utility/CriteriaUtil.java
+++ src/main/java/com/nicico/evaluation/utility/CriteriaUtil.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
