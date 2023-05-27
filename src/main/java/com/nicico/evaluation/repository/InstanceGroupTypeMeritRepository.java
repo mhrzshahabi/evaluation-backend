42c5e08204d7952f20b6082a4d269704d03e042f
amir lotfi <lotfiamir137272@gmail.com>
2023-05-27 09:25:13 +0330

    add deleteByGroupTypeMerit for delete groupTypeMerit service

--- src/main/java/com/nicico/evaluation/repository/InstanceGroupTypeMeritRepository.java
+++ src/main/java/com/nicico/evaluation/repository/InstanceGroupTypeMeritRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface InstanceGroupTypeMeritRepository extends JpaRepository<InstanceGroupTypeMerit, Long>, JpaSpecificationExecutor<InstanceGroupTypeMerit> {
     List<InstanceGroupTypeMerit> getAllByGroupTypeMeritId(Long id);
+    
+    void deleteByGroupTypeMeritId(Long groupTypeMerit);
 }
