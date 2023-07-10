96aaf50eeb4be4ca4a8e434f50b8ca32961216f7
xnamdari <you@example.com>
2023-07-10 08:57:59 +0330

    add websocket

---
+++ src/main/java/com/nicico/evaluation/websocket/SessionsManager.java
@@ -0,0 +1,30 @@
+package com.nicico.evaluation.websocket;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.Serializable;
+import java.util.*;
+@Component
+@Data
+public class SessionsManager {
+    private Map<Integer,WebSocketSession> sessions;
+
+    public SessionsManager() {
+        sessions= Collections.synchronizedMap(new HashMap<Integer, WebSocketSession>());
+    }
+
+    public void add(int id,WebSocketSession webSocketSession){
+        sessions.put(id,webSocketSession);
+    }
+
+
+    public void remove(int id){
+        sessions.remove(id);
+    }
+
+
+
+
+}
