3201fef056a3a7c7864808b16f8a06809583d736
semami <setareh.emami@gmail.com>
2023-09-25 11:07:41 +0330

    import special case

--- src/main/java/com/nicico/evaluation/repository/PersonRepository.java
+++ src/main/java/com/nicico/evaluation/repository/PersonRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>, JpaSpecificationExecutor<Person> {
 
-    Person findAllByNationalCode(String nationalCode);
+    Person findByNationalCode(String nationalCode);
     Person findByPostCode(String postCode);
+    List<Person> findAllByNationalCodeIn(List<String> nationalCode);
 
 }
