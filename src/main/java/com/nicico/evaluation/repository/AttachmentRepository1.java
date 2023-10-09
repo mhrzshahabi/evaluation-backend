548585265192872061dd12b4b38f9dd8b0e6a776
semami <setareh.emami@gmail.com>
2023-10-09 12:33:43 +0330

    change status evaluation period

---
+++ src/main/java/com/nicico/evaluation/repository/AttachmentRepository1.java
@@ -0,0 +1,13 @@
+package com.nicico.evaluation.repository;
+
+import com.nicico.evaluation.model.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AttachmentRepository1 extends JpaRepository<Attachment, Long>, JpaSpecificationExecutor<Attachment> {
+    List<Attachment> findAllByObjectIdAndObjectTypeAndGroupId(Long objectId, String objectType, String groupId);
+}
