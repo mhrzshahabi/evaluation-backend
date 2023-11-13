2f1795c22dccedad3d6fe7be2cf8bfe9700d0255
shahabi <mshahabi>
2023-11-13 16:43:43 +0330

    EVA-445

--- src/main/java/com/nicico/evaluation/mapper/SensitiveEventPersonViewMapper.java
+++ src/main/java/com/nicico/evaluation/mapper/SensitiveEventPersonViewMapper.java
@@ -6,13 +6,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = SpecialCaseMapper.class)
 public interface SensitiveEventPersonViewMapper {
 
     @Mappings({
             @Mapping(target = "eventPolicyCatalog", source = "eventPolicyCatalog.title"),
             @Mapping(target = "typeCatalog", source = "typeCatalog.title"),
-            @Mapping(target = "statusCatalog", source = "statusCatalog")
+            @Mapping(target = "statusCatalog", source = "statusCatalog"),
+            @Mapping(target = "eventDate", source = "eventDate", qualifiedByName = "convertStringToDate")
     })
     SensitiveEventsDTO.SensitiveEventPersonInfo entityToDtoInfo(SensitiveEventPersonView entity);
 
