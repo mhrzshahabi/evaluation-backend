7a4b949d0abb08c5b57fcdef12d4a5ee9eca6196
semami <setareh.emami@gmail.com>
2023-11-20 14:35:11 +0330

    evaluation view general report

--- src/main/java/com/nicico/evaluation/service/EvaluationGeneralReportService.java
+++ src/main/java/com/nicico/evaluation/service/EvaluationGeneralReportService.java
@@ -4,8 +4,7 @@ import com.nicico.copper.common.dto.search.EOperator;
 import com.nicico.copper.common.dto.search.SearchDTO;
 import com.nicico.copper.core.SecurityUtil;
 import com.nicico.evaluation.common.PageableMapper;
-import com.nicico.evaluation.dto.CatalogDTO;
-import com.nicico.evaluation.dto.EvaluationDTO;
+import com.nicico.evaluation.dto.EvaluationGeneralReportDTO;
 import com.nicico.evaluation.iservice.ICatalogService;
 import com.nicico.evaluation.iservice.IEvaluationGeneralReportService;
 import com.nicico.evaluation.iservice.IOrganizationTreeService;
@@ -14,7 +13,6 @@ import com.nicico.evaluation.repository.EvaluationViewGeneralReportRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,15 +38,13 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
 
     @Override
     @Transactional(readOnly = true)
-    @PreAuthorize("hasAuthority('R_EVALUATION')")
-    public SearchDTO.SearchRs<EvaluationDTO.Info> search(SearchDTO.SearchRq request) throws IllegalAccessException, NoSuchFieldException {
+    public SearchDTO.SearchRs<EvaluationGeneralReportDTO.Info> search(SearchDTO.SearchRq request) throws IllegalAccessException, NoSuchFieldException {
         return BaseService.optimizedSearch(repository, mapper::entityToDtoInfo, request);
     }
 
     @Override
     @Transactional(readOnly = true)
-    @PreAuthorize("hasAuthority('R_LAST_FINALIZED_EVALUATION')")
-    public SearchDTO.SearchRs<EvaluationDTO.Info> searchByParent(SearchDTO.SearchRq request) throws IllegalAccessException, NoSuchFieldException {
+    public SearchDTO.SearchRs<EvaluationGeneralReportDTO.Info> searchByParent(SearchDTO.SearchRq request) throws IllegalAccessException, NoSuchFieldException {
         List<String> postCodeList = organizationTreeService.getByParentNationalCode();
 
         final List<SearchDTO.CriteriaRq> criteriaRqList = new ArrayList<>();
@@ -76,10 +72,9 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
 
     @Override
     @Transactional(readOnly = true)
-    @PreAuthorize("hasAuthority('R_EVALUATION_COMPREHENSIVE')")
-    public SearchDTO.SearchRs<EvaluationDTO.Info> searchEvaluationComprehensive(SearchDTO.SearchRq request, int count, int startIndex) {
+    public SearchDTO.SearchRs<EvaluationGeneralReportDTO.Info> searchEvaluationComprehensive(SearchDTO.SearchRq request, int count, int startIndex) {
 
-        List<EvaluationDTO.Info> infoList = new ArrayList<>();
+        List<EvaluationGeneralReportDTO.Info> infoList = new ArrayList<>();
         String whereClause = String.valueOf(getWhereClause(request));
         Pageable pageable = pageableMapper.toPageable(count, startIndex);
         String query = getSubAssessorQuery(whereClause, pageable);
@@ -88,12 +83,12 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
             setInfoList(infoList, resultList);
             List totalResultList = entityManager.createNativeQuery(getTotalCountSubAssessorQuery(String.valueOf(whereClause))).getResultList();
             Long totalCount = !totalResultList.isEmpty() ? Long.parseLong(totalResultList.get(0).toString()) : 0;
-            SearchDTO.SearchRs<EvaluationDTO.Info> searchRs = new SearchDTO.SearchRs<>();
+            SearchDTO.SearchRs<EvaluationGeneralReportDTO.Info> searchRs = new SearchDTO.SearchRs<>();
             searchRs.setList(infoList);
             searchRs.setTotalCount(totalCount);
             return searchRs;
         } else {
-            SearchDTO.SearchRs<EvaluationDTO.Info> searchRs = new SearchDTO.SearchRs<>();
+            SearchDTO.SearchRs<EvaluationGeneralReportDTO.Info> searchRs = new SearchDTO.SearchRs<>();
             searchRs.setList(new ArrayList<>());
             searchRs.setTotalCount(0L);
             return searchRs;
@@ -102,7 +97,7 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
 
     @Override
     @Transactional(readOnly = true)
-    public SearchDTO.SearchRs<EvaluationDTO.Info> searchByPermission(SearchDTO.SearchRq request, int count, int startIndex) throws IllegalAccessException, NoSuchFieldException {
+    public SearchDTO.SearchRs<EvaluationGeneralReportDTO.Info> searchByPermission(SearchDTO.SearchRq request, int count, int startIndex) throws IllegalAccessException, NoSuchFieldException {
 
         final List<SearchDTO.CriteriaRq> criteriaRqList = new ArrayList<>();
         final SearchDTO.CriteriaRq statusCriteriaRq = new SearchDTO.CriteriaRq()
@@ -127,38 +122,30 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
         return null;
     }
 
-    private List<EvaluationDTO.Info> setInfoList(List<EvaluationDTO.Info> infoList, List<?> resultList) {
+    private List<EvaluationGeneralReportDTO.Info> setInfoList(List<EvaluationGeneralReportDTO.Info> infoList, List<?> resultList) {
         if (resultList != null) {
             for (Object evaluation : resultList) {
                 Object[] eval = (Object[]) evaluation;
-                EvaluationDTO.Info evaluationInfo = new EvaluationDTO.Info();
+                EvaluationGeneralReportDTO.Info evaluationInfo = new EvaluationGeneralReportDTO.Info();
                 evaluationInfo.setId(eval[0] == null ? null : Long.parseLong(eval[0].toString()));
                 evaluationInfo.setAssessPostCode(eval[2] == null ? null : (eval[2].toString()));
-                evaluationInfo.setAssessFullName(eval[9] == null ? null : (eval[9].toString()));
-                evaluationInfo.setAssessPostTitle(eval[28] == null ? null : (eval[28].toString()));
-                evaluationInfo.setAssessorFullName(eval[8] == null ? null : (eval[8].toString()));
-                evaluationInfo.setAssessorPostCode(eval[4] == null ? null : (eval[4].toString()));
-                evaluationInfo.setAssessorPostTitle(eval[30] == null ? null : (eval[30].toString()));
-                EvaluationDTO.EvaluationPeriodTupleDTO evaluationPeriod = new EvaluationDTO.EvaluationPeriodTupleDTO();
-                evaluationInfo.setEvaluationPeriodId(eval[6] == null ? null : (Long.parseLong(eval[6].toString())));
-                evaluationPeriod.setId(eval[6] == null ? null : (Long.parseLong(eval[6].toString())));
-                evaluationPeriod.setTitle(eval[31] == null ? null : (eval[31].toString()));
-                evaluationPeriod.setStartDateAssessment(eval[32] == null ? null : (eval[32].toString()));
-                evaluationPeriod.setEndDateAssessment(eval[33] == null ? null : (eval[33].toString()));
-                evaluationInfo.setEvaluationPeriod(evaluationPeriod);
-                evaluationInfo.setAverageScore(eval[7] == null ? null : Long.parseLong(eval[7].toString()));
-                evaluationInfo.setStatusCatalogId(eval[12] == null ? null : Long.parseLong(eval[12].toString()));
-                evaluationInfo.setPostGradeTitle(eval[25] == null ? null : (eval[25].toString()));
-                evaluationInfo.setMojtamaTitle(eval[21] == null ? null : (eval[21].toString()));
-                evaluationInfo.setMoavenatTitle(eval[20] == null ? null : (eval[20].toString()));
-                evaluationInfo.setOmoorTitle(eval[22] == null ? null : (eval[22].toString()));
-                evaluationInfo.setGhesmatTitle(eval[18] == null ? null : (eval[18].toString()));
-                evaluationInfo.setDescription(eval[11] == null ? null : (eval[11].toString()));
-                CatalogDTO.Info statusCatalog = new CatalogDTO.Info();
-                statusCatalog.setId(eval[12] == null ? null : (Long.parseLong(eval[12].toString())));
-                statusCatalog.setTitle(eval[34] == null ? null : (eval[34].toString()));
-                statusCatalog.setCode(eval[35] == null ? null : (eval[35].toString()));
-                evaluationInfo.setStatusCatalog(statusCatalog);
+                evaluationInfo.setEvaluationPeriodId(eval[5] == null ? null : (Long.parseLong(eval[5].toString())));
+                evaluationInfo.setAverageScore(eval[6] == null ? null : Long.parseLong(eval[6].toString()));
+                evaluationInfo.setAssessFullName(eval[8] == null ? null : (eval[8].toString()));
+                evaluationInfo.setGhesmatTitle(eval[10] == null ? null : (eval[10].toString()));
+                evaluationInfo.setMoavenatTitle(eval[11] == null ? null : (eval[11].toString()));
+                evaluationInfo.setMojtamaTitle(eval[12] == null ? null : (eval[12].toString()));
+                evaluationInfo.setOmoorTitle(eval[13] == null ? null : (eval[13].toString()));
+                evaluationInfo.setCostCenterCode(eval[15] == null ? null : (eval[15].toString()));
+                evaluationInfo.setCostCenterTitle(eval[16] == null ? null : (eval[16].toString()));
+                evaluationInfo.setPersonnelCode(eval[17] == null ? null : (eval[17].toString()));
+                evaluationInfo.setAvgBehavioral(eval[18] == null ? null : (eval[18].toString()));
+                evaluationInfo.setWeightBehavioral(eval[19] == null ? null : (eval[19].toString()));
+                evaluationInfo.setAvgDevelopment(eval[20] == null ? null : (eval[20].toString()));
+                evaluationInfo.setWeightDevelopment(eval[21] == null ? null : (eval[21].toString()));
+                evaluationInfo.setAvgOperational(eval[22] == null ? null : (eval[22].toString()));
+                evaluationInfo.setWeightOperational(eval[23] == null ? null : (eval[23].toString()));
+                evaluationInfo.setCountItem(eval[24] == null ? null : (Long.parseLong(eval[24].toString())));
                 infoList.add(evaluationInfo);
             }
         }
@@ -169,16 +156,9 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
         if (Objects.nonNull(pageable))
             whereClause = MessageFormat.format("{0}{1}", whereClause, String.format(" OFFSET %s ROWS FETCH NEXT %s ROWS ONLY", pageable.getPageNumber() * pageable.getPageSize(), pageable.getPageSize()));
         return String.format(" SELECT  " +
-                        "                 eval.*, " +
-                        "                 evalPeriod.c_title, " +
-                        "                 evalPeriod.c_start_date_assessment, " +
-                        "                 evalPeriod.c_end_date_assessment,  " +
-                        "                 catalog.c_title status_catalog_title, " +
-                        "                 catalog.c_code status_catalog_code " +
+                        "                 eval.* " +
                         "            FROM  " +
                         "                view_evaluation_general_report  eval" +
-                        "                join tbl_evaluation_period evalPeriod on evalPeriod.id = eval.evaluation_period_id " +
-                        "                join tbl_catalog             catalog ON catalog.id = eval.status_catalog_id " +
                         "            WHERE  " +
                         "                c_assessor_post_code IN (  " +
                         "                    SELECT  " +
@@ -214,7 +194,6 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
                 "                 count(eval.id) " +
                 "            FROM  " +
                 "                view_evaluation_general_report  eval" +
-                "                join tbl_evaluation_period evalPeriod on evalPeriod.id = eval.evaluation_period_id " +
                 "               join tbl_catalog             catalog ON catalog.id = eval.status_catalog_id " +
                 "            WHERE  " +
                 "                c_assessor_post_code IN (  " +
@@ -252,20 +231,13 @@ public class EvaluationGeneralReportService implements IEvaluationGeneralReportS
                     case "assessFullName" -> whereClause.append(" and ").append("eval.c_assess_full_name").append(" like '%").append(criteria.getValue().toString()).append("%'");
                     case "assessPostCode" -> whereClause.append(" and ").append("eval.c_assess_post_code").append(" like '%").append(criteria.getValue()).append("%'");
                     case "assessPostTitle" -> whereClause.append(" and ").append("eval.post_title").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "assessorFullName" -> whereClause.append(" and ").append("eval.c_assessor_full_name").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "assessorPostCode" -> whereClause.append(" and ").append("eval.c_assessor_post_code").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "assessorPostTitle" -> whereClause.append(" and ").append("eval.assessor_post_title").append(" like '%").append(criteria.getValue()).append("%'");
                     case "evaluationPeriod.id" -> whereClause.append(" and ").append("evalPeriod.id").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "evaluationPeriod.startDateAssessment" -> whereClause.append(" and ").append("evalPeriod.c_start_date_assessment").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "evaluationPeriod.endDateAssessment" -> whereClause.append(" and ").append("evalPeriod.c_end_date_assessment").append(" like '%").append(criteria.getValue()).append("%'");
                     case "averageScore" -> whereClause.append(" and ").append("eval.average_score").append(" like '%").append(criteria.getValue()).append("%'");
                     case "postGradeTitle" -> whereClause.append(" and ").append("eval.post_grade_title").append(" like '%").append(criteria.getValue()).append("%'");
                     case "mojtamaTitle" -> whereClause.append(" and ").append("eval.mojtama_title").append(" like '%").append(criteria.getValue()).append("%'");
                     case "moavenatTitle" -> whereClause.append(" and ").append("eval.moavenat_title").append(" like '%").append(criteria.getValue()).append("%'");
                     case "omoorTitle" -> whereClause.append(" and ").append("eval.omoor_title").append(" like '%").append(criteria.getValue()).append("%'");
                     case "ghesmatTitle" -> whereClause.append(" and ").append("eval.ghesmat_title").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "description" -> whereClause.append(" and ").append("eval.c_description").append(" like '%").append(criteria.getValue()).append("%'");
-                    case "statusCatalog.id" -> whereClause.append(" and ").append("eval.status_catalog_id").append(" like '%").append(criteria.getValue()).append("%'");
                 }
             });
         return new StringBuilder(String.valueOf(whereClause).replaceAll("\\[|\\]", ""));
